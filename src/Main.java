32ea0de0cbfff3c109860f8e5c554d88f49abf39
kaVII-x <ka52jiguli@yandex.ru>
2022-12-21 23:23:57 +0200

    Первле задание

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       int apple = 10;
+       byte ram = 124;
+       short intelIdea = 34;
+       float magaHeart = 23f;
+       double heartLife = 100;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        System.out.println("Значение переменной ram с типом byte равно " + ram);
+        System.out.println("Значение переменной intelIdea с типом short равно " + intelIdea);
+        System.out.println("Значение переменной megaHeart с типом float равно " + magaHeart);
+        System.out.println("Значение переменной heartLife с типом double равно " + heartLife);
+
+
+    }
+}
